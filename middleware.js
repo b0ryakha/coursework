1d9a506b356fb6de3ef47a5cbaa69609ca743cbd
b0ryakha <tosha.mar4encko@yandex.ru>
2024-11-19 20:59:11 +0700

    fix

--- middleware.js
+++ middleware.js
@@ -3,16 +3,16 @@ const jwt = require("jsonwebtoken")
 class Middleware {
     static async auth(req, res, next) {
         const token = req.cookies.token
-        if (!token) return res.status(401).json("Пользователь не авторизован")
+        if (!token) return res.json("Пользователь не авторизован")
 
         jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-            if (err) return res.status(403).json("Пользователь не авторизован")
+            if (err) return res.json("Пользователь не авторизован")
             req.user = user
             next()
         })
     }
 
-    static async ident(req, res, next) {
+    static async ident(req, _, next) {
         const token = req.cookies.token
         if (!token) return next()
 
@@ -22,7 +22,7 @@ class Middleware {
         })
     }
 
-    static async contentUnlock(req, res, next) {
+    static async contentUnlock(_, res, next) {
         res.setHeader("Content-Security-Policy", "script-src 'self' https://getbootstrap.com;")
         next()
     }
